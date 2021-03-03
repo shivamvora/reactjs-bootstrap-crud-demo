7f88238c03ce4c5cfce5e16fb8333d089cbe4827
shivamvora <shivampatel27102001@gmail.com>
2021-03-03 21:43:15 +0530

    deployed

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,12 +4,13 @@ const About = () => {
     return (
         <div className="container">
             <div className="py-4">
-                <h1>About Page</h1>
-                <p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum voluptate tenetur similique aliquam ab cumque odio eveniet saepe quia, fuga nihil tempora et animi sed non est ad dolores quo.</p>
+                <div className="jumbotron bg-primary">
+                    <div className="text-white">
+                        Our mission is to be the most trusted and reliable technology partner for organizations
+                        We are a team of highly experienced engineers backed by our product portfolio in providing innovative technology solutions for our client’s. Our products and solutions enable organizations to innovate and transform their operational efficiencies, accelerate their growth and increase revenue. Leveraging our TechVision+ Platform utilizes ML/AI, Intelligent Vision and IoT to accelerate our client solution delivery, mitigate risk and transform the way they operate.
+                    </div>
+                </div>
 
-                <p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum voluptate tenetur similique aliquam ab cumque odio eveniet saepe quia, fuga nihil tempora et animi sed non est ad dolores quo.</p>
-
-                <p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum voluptate tenetur similique aliquam ab cumque odio eveniet saepe quia, fuga nihil tempora et animi sed non est ad dolores quo.</p>
             </div>
         </div>
     )
