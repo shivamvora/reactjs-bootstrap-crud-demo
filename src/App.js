4c4d1aa97a612e9db750c953c26da438866d5d63
shivamvora <shivampatel27102001@gmail.com>
2021-03-03 11:12:36 +0530

    json server added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NotFound from './components/pages/NotFound';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
+          <Route component={NotFound} />
         </Switch>
       </div>
     </Router>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h1 className="display-1">
+                Page Not Found Sorry Dear....
+            </h1>
+        </div>
+    )
+}
+
+export default NotFound;
